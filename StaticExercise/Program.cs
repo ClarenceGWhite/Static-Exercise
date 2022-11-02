ddf2f5f9b566bc73ee309b9ceb9a1c255a41091d
clarence <cgwhite76@gmail.com>
2022-11-01 19:23:12 -0500

    Final Version

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace StaticExercise
+﻿using StaticExercise;
+namespace StaticExercise
 {
+
+    
     public class Program
     {
-        static void Main(string[] args)
+       
+
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("32 degrees F equals this Celsius value... ");
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(32));
+
+            Console.WriteLine();
+
+            Console.WriteLine("100 degrees C equals this Fahrenheit value...");
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(100));
+       
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+
+        public static double FahrenheitToCelsius(double fahrenheitTemp)
+        {
+            // C = (F - 32) * 5 / 9;
+            return (fahrenheitTemp - 32) * 5 / 9;
+               
+        }
+
+        public static double CelsiusToFahrenheit(double celsiusTemp) 
+        {
+            // F = (C * 9)/5 + 32
+            return (celsiusTemp * 9) / 5 + 32;
+                
+        }
+
+
+
+
+
+    }
+}
